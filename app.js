9dc3e7506ff5bb19c828f9d30dc029ce9a26d18b
Natalia Titova <bnmlucky@gmail.com>
2020-07-07 19:16:15 -0400

    got all the images to show according to the word the user types

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var form = document.querySelector("form");
+const form = document.querySelector("form");
 const input = document.querySelector('input');
 
 form.addEventListener("submit", function (event) {
@@ -8,7 +8,30 @@ form.addEventListener("submit", function (event) {
     const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=2pbUP2ptCDDc5PPyxrm26V8RwwK8D93i&q=${newImage}&limit=50&offset=0&rating=g&lang=en`;
     console.log(endpoint);
 
+    fetch(endpoint)
+        .then(response => response.json())
+        .then(data => showImages(data));
+
+    function showImages(data) {
+        for (let i = 0; i < 50; i++) {
+            const image = document.createElement('img');
+            image.setAttribute('src', data.data[i].images.preview_gif.url);
+
+            //document.body.appendChild(image);
+
+            document.getElementById("allimages").appendChild(image);
+
+            //     .setAttribute("src", data.data[i].images.preview_gif.url);
+            // const imageDiv = document.getElementById("allimages").appendChild('img');
+            // console.log(imageDiv);
+        }
+    }
+
+
+
 });
+
+
 
 
 
