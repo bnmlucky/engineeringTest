18417fffd2878ee91395e35cce16cdd2f7e19363
Natalia Titova <bnmlucky@gmail.com>
2020-07-07 13:06:19 -0400

    I can filter giphys by the form input

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ form.addEventListener("submit", function (event) {
     event.preventDefault();
     const newImage = input.value;
     console.log(newImage);
+    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=2pbUP2ptCDDc5PPyxrm26V8RwwK8D93i&q=${newImage}&limit=50&offset=0&rating=g&lang=en`;
+    console.log(endpoint);
 
 });
 
