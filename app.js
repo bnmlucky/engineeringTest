1c857367ce495e18eb0f9241ec808065dc962a90
Natalia Titova <bnmlucky@gmail.com>
2020-07-09 16:19:57 -0400

    tried a solution for pagination, but was unable to get it to work - will leave it there as an idea that I've tried

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const form = document.querySelector("form");
 const input = document.querySelector('input');
+const images = [];
+const allImagesDiv = document.getElementById("allimages");
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const page = document.querySelector(".page-num");
+const maxItem = 10;
+let index = 1;
 
 form.addEventListener("submit", function (event) {
     event.preventDefault();
@@ -14,27 +21,84 @@ form.addEventListener("submit", function (event) {
         .catch(error => console.log('Looks like there was a problem', error));
 
     function showImages(data) {
-        const allImagesDiv = document.getElementById("allimages");
+
         while (allImagesDiv.firstChild) allImagesDiv.removeChild(allImagesDiv.firstChild);
 
         for (let i = 0; i < 50; i++) {
             const image = document.createElement('img');
             image.setAttribute('src', data.data[i].images.preview_gif.url);
+            // image.setAttribute("class", "hide"); - only one class can be added at creation of these elements
+            // image.setAttribute("class", "show"); - but not 2
+
             allImagesDiv.appendChild(image);
         }
+    }
+
+    //My attempt at pagination that I couldn't make work 
+
+    const allImages = allImagesDiv.children;
+    const pagination = Math.ceil(allImages.length / maxItem);
+    console.log(pagination);
+
+    prev.addEventListener("click", function () {
+        index--;
+        check();
+        showItems();
+    })
+
+    next.addEventListener("click", function () {
+        index++;
+        check();
+        showItems();
+    })
+
+    function check() {
+        if (index == pagination) {
+            next.classList.add("disabled");
+        } else {
+            next.classList.remove("disabled");
+        }
+
+        if (index == 1) {
+            prev.classList.add("disabled");
+        } else {
+            prev.classList.remove("disabled");
+        }
+    }
+
+    function showItems() {
+        for (let i = 0; i < allImages.length; i++) {
+            allImages[i].classList.remove("show");
+            allImages[i].classList.add("hide"); //this is where I can't add a class to a dynamically generated image
+            if (i >= (index * maxItem) - maxItem && i < index * maxItem) {
+                allImages[i].classList.remove("hide");
+                allImages[i].classList.add("show");
+            }
+            page.innerHTML = index;
+        }
+    }
+
+    window.onload = function () {
+        showItems();
+        check();
     }
 
     const reloadPage = () => {
         document.getElementById("future-query").addEventListener("click", event => {
             location.reload(true);
-            //document.getElementById("future-query").focus();
         })
     }
 
     reloadPage();
     input.focus();
 
+
 });
+
+
+
+
+
 
 
 
