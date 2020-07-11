88b6c5f2599c18381f9b4ecc0b9a690772e66ec7
Natalia Titova <bnmlucky@gmail.com>
2020-07-11 19:39:51 -0400

    fixed a bug with pagination and restyled images in the grid, images on the modal, and input field

--- app.js
+++ app.js
@@ -17,12 +17,84 @@ let currentPage = 0;
 const allImagesDiv = document.getElementById("allimages");
 const modal = document.getElementById('simpleModal');
 const closeBtn = document.querySelector('.closeBtn');
-const modalContent = document.querySelector(".modal-content")
-const modalImg = document.getElementById("modalImg")
+const modalContent = document.querySelector(".modal-content");
+const modalImg = document.getElementById("modalImg");
 
-const pageNumberElement = document.getElementById('pageNumber')
+const pageNumberElement = document.getElementById('pageNumber');
 
-form.addEventListener("submit", function(event) {
+function setupModal(img, url) {
+    img.addEventListener('click', () => {
+        modal.style.display = 'block';
+        modalImg.setAttribute('src', url);
+    });
+
+    // listen for close click
+    closeBtn.addEventListener("click", () => {
+        modal.style.display = 'none';
+    });
+
+    // listen for outside click
+    window.addEventListener('click', (event) => {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    });
+}
+
+function setupPagination() {
+    prev.addEventListener("click", () => {
+        currentPage = Math.max(currentPage - 1, 0);
+        showPage();
+    })
+
+    next.addEventListener("click", () => {
+        currentPage = Math.min(currentPage + 1, maxPage);
+        showPage();
+    })
+}
+
+function updatePaginationElements() {
+    paginationDiv.style.visibility = 'visible';
+    pageNumberElement.textContent = `Page ${currentPage + 1} of ${maxPage + 1}`
+
+    if (currentPage == maxPage) {
+        next.classList.add("disabled");
+    } else {
+        next.classList.remove("disabled");
+    }
+
+    if (currentPage == 0) {
+        prev.classList.add("disabled");
+    } else {
+        prev.classList.remove("disabled");
+    }
+}
+
+function showPage() {
+    removeImages();
+    addImages();
+    updatePaginationElements();
+}
+
+function removeImages() {
+    while (allImagesDiv.firstChild)
+        allImagesDiv.removeChild(allImagesDiv.firstChild);
+}
+
+function addImages() {
+    const elementsOnPage = images.slice(currentPage * elementsPerPage, (currentPage + 1) * elementsPerPage)
+
+    elementsOnPage.forEach(element => {
+        const img = document.createElement('img');
+        img.setAttribute('src', element.images.fixed_height.url);
+
+        allImagesDiv.appendChild(img);
+
+        setupModal(img, element.images.original.url);
+    });
+}
+
+form.addEventListener("submit", (event) => {
     event.preventDefault();
     const query = input.value;
     const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=2pbUP2ptCDDc5PPyxrm26V8RwwK8D93i&q=${query}&limit=${totalElements}&offset=0&rating=g&lang=en`;
@@ -37,79 +109,7 @@ form.addEventListener("submit", function(event) {
         currentPage = 0;
         showPage();
     }
-
-    function showPage() {
-        removeImages();
-        addImages();
-        updatePaginationElements();
-    }
-
-    function removeImages() {
-        while (allImagesDiv.firstChild)
-            allImagesDiv.removeChild(allImagesDiv.firstChild);
-    }
-
-    function addImages() {
-        const elementsOnPage = images.slice(currentPage * elementsPerPage, (currentPage + 1) * elementsPerPage)
-
-        elementsOnPage.forEach(element => {
-            const img = document.createElement('img');
-            img.setAttribute('src', element.images.fixed_height.url);
-
-            allImagesDiv.appendChild(img);
-
-            setupModal(img, element.images.original.url);
-        });
-    }
-
-    function setupPagination() {
-        prev.addEventListener("click", () => {
-            currentPage = Math.max(currentPage - 1, 0);
-            showPage();
-        })
-
-        next.addEventListener("click", () => {
-            currentPage = Math.min(currentPage + 1, maxPage);
-            showPage();
-        })
-    }
-
-    function updatePaginationElements() {
-        paginationDiv.style.visibility = 'visible';
-        pageNumberElement.textContent = `Page ${currentPage + 1} of ${maxPage + 1}`
-
-        if (currentPage == maxPage) {
-            next.classList.add("disabled");
-        } else {
-            next.classList.remove("disabled");
-        }
-
-        if (currentPage == 0) {
-            prev.classList.add("disabled");
-        } else {
-            prev.classList.remove("disabled");
-        }
-    }
-
-    function setupModal(img, url) {
-        img.addEventListener('click', () => {
-            modal.style.display = 'block';
-            modalImg.setAttribute('src', url);
-        });
-
-        // listen for close click
-        closeBtn.addEventListener("click", () => {
-            modal.style.display = 'none';
-        });
-
-        // listen for outside click
-        window.addEventListener('click', (event) => {
-            if (event.target == modal) {
-                modal.style.display = 'none';
-            }
-        });
-    }
-
-    setupPagination();
-    input.focus();
 });
+
+setupPagination();
+input.focus();
